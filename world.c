3e3217f9a32ad9c69b06dc625363fa4e9677baf8
Tomáš Režňák <tomas.reznak@volny.cz>
2023-04-05 21:28:59 +0200

    Add wall types

--- world.c
+++ world.c
@@ -13,47 +13,56 @@ const struct wall_t world_walls[WORLD_NWALLS] = {
         {
                 {0,   0},
                 {SCREEN_WIDTH - 1, 0},
-                RED
+                RED,
+                WALL_TYPE_SOLID
         },
         {
                 {SCREEN_WIDTH - 1, 0},
                 {SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1},
-                GREEN
+                GREEN,
+                WALL_TYPE_SOLID
         },
         {
                 {SCREEN_WIDTH - 1, SCREEN_HEIGHT - 1},
                 {0,                SCREEN_HEIGHT - 1},
-                BLUE
+                BLUE,
+                WALL_TYPE_SOLID
         },
         {
                 {0,   0},
                 {0,                SCREEN_HEIGHT - 1},
-                CYAN
+                CYAN,
+                WALL_TYPE_NONSOLID
         },
         {
                 {100, 100},
                 {200,  100},
-                BLUE
+                BLUE,
+                WALL_TYPE_NONSOLID
         },
         {
                 {100, 100},
                 {100,  200},
-                BLUE
+                BLUE,
+                WALL_TYPE_NONSOLID
         },
         {
                 {200, 100},
                 {200,  200},
-                MAGENTA
+                MAGENTA,
+                WALL_TYPE_NONSOLID
         },
         {
                 {200, 200},
                 {100,  200},
-                BLUE
+                BLUE,
+                WALL_TYPE_NONSOLID
         },
         {
                 {500, 1000},
                 {1000, 800},
-                YELLOW
+                YELLOW,
+                WALL_TYPE_MIRROR
         }
 };
 
--- world.h
+++ world.h
@@ -2,16 +2,29 @@
 #define RAY_WORLD_H
 
 
+#include <stdbool.h>
+
+
 #include "vector.h"
 
 
 #define WORLD_NWALLS 100
 
 
+#define WALL_TYPE_SOLID (1 << 0)
+#define WALL_TYPE_NONSOLID (1 << 1)
+#define WALL_TYPE_MIRROR (1 << 2)
+#define WALL_TYPE_PORTAL (1 << 3)
+
+
+#define WALLL_TYPE_DEFAULT WALL_TYPE_SOLID
+
+
 struct wall_t {
     struct vector_t a;
     struct vector_t b;
     SDL_Color color;
+    unsigned int type;
 };
 
 
