fd2f2584b94de37d1520a115f35e419f8c9b800a
Tomáš Režňák <tomas.reznak@volny.cz>
2023-08-23 20:33:50 +0200

    Refactor vector computations in render_camera()

--- src/game.c
+++ src/game.c
@@ -164,13 +164,11 @@ private void render_camera(const struct game_t *const game) {
                       5,
                       0xFF0000FF);
 
+    struct vec_t *const pos = vcopy(&game->camera->pos);
+    struct vec_t *const dir = vcopy(&game->camera->dir);
+    const struct vec_t *const endpoint = vadd(pos, vmul(dir, 100.0F));
+
     set_color(game, COLOR_GREEN);
-
-    const struct vec_t *const endpoint = vadd(
-            vmove(vector(), &game->camera->pos),
-            vmul(vmove(vector(), &game->camera->dir), 100)
-    );
-
     SDL_RenderDrawLineF(game->renderer, game->camera->pos.x, game->camera->pos.y, endpoint->x, endpoint->y);
 }
 
