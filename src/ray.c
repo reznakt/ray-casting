f6c25416ca323555128f507a54a12fcc0e9becb2
Tomáš Režňák <tomas.reznak@volny.cz>
2024-01-06 12:50:07 +0100

    Use uppercase 'F' in float literals; use isclose() instead of '=='

--- src/ray.c
+++ src/ray.c
@@ -1,6 +1,9 @@
 #include <stdlib.h>
 
 
+#include "math.h"
+
+
 #include "ray.h"
 
 
@@ -21,14 +24,14 @@ struct vec_t *ray_intersection(const struct ray_t *const restrict ray,
 
     const float den = (x1 - x2) * (y3 - y4) - (y1 - y2) * (x3 - x4);
 
-    if (den == 0.0f) {
+    if (isclose(den, 0.0F)) {
         return NULL;
     }
 
     const float t = ((x1 - x3) * (y3 - y4) - (y1 - y3) * (x3 - x4)) / den;
     const float u = ((x2 - x1) * (y1 - y3) - (y2 - y1) * (x1 - x3)) / den;
 
-    if (t > 0.0f && t < 1.0f && u > 0.0f) {
+    if (t > 0.0F && t < 1.0F && u > 0.0F) {
         dst->x = x3 + u * dx;
         dst->y = y3 + u * dy;
         return dst;
